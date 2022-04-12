7ed28f0de31d415a83a2411236aca3845909e4a5
pstachula <pawe.stachula.mail@gmail.com>
2022-04-12 09:39:42 +0200

    rename folder

--- packages/frontend/modules/books/Books.tsx
+++ packages/frontend/modules/books/Books.tsx
@@ -1,18 +1,10 @@
-import { Book } from "../../utils/types/books.types";
-import { memo, useState } from "react";
-import { useBookStore } from "../../utils/stores/books-store"
-
-const BookChild = ({ books }: { books?: Book[]}) => {
-    return <div>Book child</div>
-};
+import { useBookStore } from "../../services/stores/books-store"
 
 export const Books = () => {
-    const [_state, setState] = useState(0);
     const { books, postBook } = useBookStore(); 
 
     const addNewBook = () => {
         postBook({ name: 'New Book', pages: 123 });
-        setState(Math.random());
     }          
     
     return <div>
@@ -21,6 +13,5 @@ export const Books = () => {
             {books?.map(book => <li key={book.name}>{book.name}</li>)}
         </ul>
         <button onClick={addNewBook}>Add new Book</button>
-        <BookChild books={books} />
     </div>
 }
--- packages/frontend/modules/books/BooksProvider.tsx
+++ packages/frontend/modules/books/BooksProvider.tsx
@@ -1,12 +1,12 @@
 import { useEffect, ReactNode } from "react";
-import { useBookStore } from "../../utils/stores/books-store";
+import { useBookStore } from "../../services/stores/books-store";
 
 export const BooksProvider = ({ children }: { children: ReactNode }) => {
     const { getPageData } = useBookStore()
     
     useEffect(() => {
         getPageData();
-    }, [getPageData]);
+    }, []);
     
     return <>{children}</>
 }
